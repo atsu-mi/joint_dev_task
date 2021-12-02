27dae9fb946064e641c567f99a45db0843f5f0b6
atsu-mi <atumi0205@gmail.com>
2021-12-02 15:48:58 +0900

    ruby課題応用Q17~19を提出

--- task.rb
+++ task.rb
@@ -166,7 +166,18 @@ end
 
 class UserQ17
   # 以下に回答を記載
-
+  def initialize(name:, age:, gender:)
+    @name = name
+    @age = age
+    @gender = gender
+  end
+  def info
+    puts <<~TEXT
+    名前:#{@name}
+    年齢:#{@age}
+    性別:#{@gender}
+    TEXT
+  end
 end
 
 def q17
@@ -181,7 +192,16 @@ end
 
 class UserQ18
   # 以下に回答を記載
-
+  def initialize(name:, age:)
+    @name = name
+    @age = age
+  end
+  def introduce
+    if @age >= 20
+      "こんにちは，#{@name}と申します。宜しくお願いいたします。"
+    else "はいさいまいど〜，#{@name}です！！！"
+    end
+  end
 end
 
 def q18
@@ -195,8 +215,8 @@ end
 
 class Item
   # 以下を修正して下さい
-
-  def initialize(name)
+  attr_reader :name
+  def initialize(name:)
     @name = name
   end
 end
@@ -209,7 +229,6 @@ end
 
 class UserQ20
   # 以下に回答を記載
-
 end
 
 class Zoo
